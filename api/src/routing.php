b43c2f9e19f0cfcad4909a6c4dbc842586ccc0c0
Ahmad Nur Fais <adhsteiz@gmail.com>
2024-03-23 22:30:47 +0700

    Updated the routing to handle the index cases

--- api/src/routing.php
+++ api/src/routing.php
@@ -1,4 +1,6 @@
 <?php
+require_once './autoload.php';
+
 if ($_SERVER['SERVER_ADDR'] == '127.0.0.1' || $_SERVER['SERVER_ADDR'] == '::1') {
     $url = ($_SERVER['REQUEST_SCHEME'] == 'http') ? "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" : null;
 } else {
@@ -10,7 +12,13 @@ $page = rtrim(ltrim($page, '/'), '/');
 $page = str_replace('src', 'pages', $page);
 // Note that in local, it would be localhost/cribster/api/ ...
 // While in the hosting, it would be / ...
-$page = ($_SERVER['SERVER_ADDR'] == '127.0.0.1' || $_SERVER['SERVER_ADDR'] == '::1') ? "../../../$page.php" : "../$page.php";
+if (strstr($page, 'pages') === substr($page, -5)) { // Check if it is the index page of the pages dir, it is ended with 'pages'
+    $page = ($_SERVER['SERVER_ADDR'] == '127.0.0.1' || $_SERVER['SERVER_ADDR'] == '::1') ? "../../../$page/index.php" : "../$page/index.php";
+} elseif ($page == '' || $page == 'cribster/api') { // Check if it is the index page
+    $page = BASE_PATH . "/pages/index.php";
+} else {
+    $page = ($_SERVER['SERVER_ADDR'] == '127.0.0.1' || $_SERVER['SERVER_ADDR'] == '::1') ? "../../../$page.php" : "../$page.php";
+}
 echo $page;
 
 if (file_exists($page)) {
