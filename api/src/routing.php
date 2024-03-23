caf90c5490948520f1c372c2a5e263e9ca560a2e
Ahmad Nur Fais <adhsteiz@gmail.com>
2024-03-23 14:57:37 +0700

    Added the routing and .htaccess file
    These files will handle the routing process whenever there is a request
    to the API endpoint

---
+++ api/src/routing.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER['SERVER_ADDR'] == '127.0.0.1' || $_SERVER['SERVER_ADDR'] == '::1') {
+    $url = ($_SERVER['REQUEST_SCHEME'] == 'http') ? "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" : null;
+} else {
+    $url = ($_SERVER['REQUEST_SCHEME'] == 'https') ? "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" : null;
+}
+
+$page = parse_url($url, PHP_URL_PATH);
+$page = rtrim(ltrim($page, '/'), '/');
+$page = str_replace('src', 'pages', $page);
+// Note that in local, it would be localhost/cribster/api/ ...
+// While in the hosting, it would be / ...
+$page = ($_SERVER['SERVER_ADDR'] == '127.0.0.1' || $_SERVER['SERVER_ADDR'] == '::1') ? "../../../$page.php" : "../$page.php";
+echo $page;
+
+if (file_exists($page)) {
+    include ($page);
+} else {
+    http_response_code(404);
+}
